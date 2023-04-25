aefce67b12865d8d433b41d26c3fcd50ed728451
jcsgirish <jcsgirish7@gmail.com>
2023-04-25 23:33:19 +0530

    Building Blocks of Ecommerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {Button} from 'react-bootstrap'
+import Header from './NavBar/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const  App=()=> {
+
+      const productsArr = [
+
+{
+
+title: 'Colors',
+
+price: 100,
+
+imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+
+},
+
+{
+
+title: 'Black and white Colors',
+
+price: 50,
+
+imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+
+},
+
+{
+
+title: 'Yellow and Black Colors',
+
+price: 70,
+
+imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+
+},
+
+{
+
+title: 'Blue Color',
+
+price: 100,
+
+imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+
+}
+
+]
+
+return(
+ 
+  <>
+   <Header></Header>
+  <ul>{productsArr.map((item,i)=>(
+        <li key={i}>
+         <h3>{item.title}</h3>
+         <h3>${item.price}</h3>
+        <img src={item.imageUrl} ></img>
+        <Button>Add To Cart</Button>
+        </li>
+      ))}</ul>
+  </>
+)
 }
 
 export default App;
